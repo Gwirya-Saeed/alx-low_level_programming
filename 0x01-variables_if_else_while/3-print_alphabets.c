4b4063878432c473de47a94013e7b626311a6b94
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-10 11:35:46 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		 (d <= 'Z') {
+			 putchar(d);
+			 d++;
+}
+	 putchar('\n');
+return (0);
+}
