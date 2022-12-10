6a69fa50c60768121497f1e5e3a67c56e7e0d1f5
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-10 15:14:07 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,9 +15,9 @@ int main(void)
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
 
-			putchar('0' + i % 10);
+			putchar(' ');
 
-			putchar('0' + i % 10);
+			putchar('0' + j / 10);
 			putchar('0' + j % 10);
 			if (i == 98 && j == 99)
 				break;
