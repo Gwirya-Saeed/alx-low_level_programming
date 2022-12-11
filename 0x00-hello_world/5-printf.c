712b8c15797ba93adda2c6aca81476c8f9a24256
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-11 05:07:09 -0800

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-printf("with proper grammar,but the out come is a piece of art,\n");
+printf("with proper grammar , but the out come is a piece of art ,"\n"");
 return (0);
 }
