96d0ef4de343b461870691253c09939156a55b06
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-11 10:58:22 -0800

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-{ #include <stdio.h>
-
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-
+nclude < stdio.h >
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 1 (Success)
+**/
+intmain(void)
+{
+write(2, "and thatpiece of artisuseful" - Dora Korpar, 2015 - 10 - 19\n ", 61);
 return (1);
-} }
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+nclude < stdio.h >
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+int main(void)
+{	
+inta; long intb; longlong intc; chard; floatf;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
