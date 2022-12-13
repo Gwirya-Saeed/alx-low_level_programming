b82304aa8eeb5dc3bb75acfdad75a1a8bf2958c2
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-13 11:45:47 -0800

    Task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+** main - check the code.
+ **
+ ** Return: Always 0.
+**/
+int main(void)
+{
+if ((c >= 65 && c <= 90) || ((c <= 97 && c <= 122))
+return (1);
+return (0);
+}
