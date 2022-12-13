f54e1aa1f1aa247492713772572449bdffd37962
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-13 05:10:06 -0800

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * main - Entry point
+ **
+ ** Return: Always 0 (Success)
+**/
+int main(void)
+{
+char putch[] = "_putchar";
+inti = 0;
+while (putch[i] != '\0')
+{
+_putchar (putch[i]);
+i++;
+}
+_putchar ('\n');
+return (0);
+}
