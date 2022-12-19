1fbbd57a290ec76d0b6c4ddba605f07f1256fcc0
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-19 05:55:08 -0800

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * puts - prints every other character of a string
+ * puts2 - prints every other character of a string
  * @str: char to check
  * Return: 0 is success
  */
 
-void puts(char *str)
+void puts2(char *str)
 {
 	int string;
 
