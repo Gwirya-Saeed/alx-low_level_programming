d62c8ef92e2cf1e78991a66974e3dcb781d39cce
Gwirya_Saeed <gwirya1995@gmail.com>
2022-12-19 09:30:53 -0800

    Task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -25,7 +25,7 @@ int main(void)
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 27722) / 2;
+		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 
