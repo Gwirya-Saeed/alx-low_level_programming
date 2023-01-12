6333f1075cd4d190b36a0fc81c5fa0e3db80472c
Gwirya_Saeed <gwirya1995@gmail.com>
2023-01-12 04:52:06 -0800

    Task0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _print(char *str, int 1);
 char *mull(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
-void init(char *str, int 1);
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
